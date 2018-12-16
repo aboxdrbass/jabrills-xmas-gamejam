953644764788f0e76f2f9dbbb2e3649d1e52788b
Turkey <N/A>
2018-12-16 04:51:40 +0300

    added Basic Controles
    added Basic Controles for the Player

---
+++ FireWall Breached/Assets/Scripts/PlayerScripts/Controls.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Controls : MonoBehaviour
+{
+
+
+    public Camera worldCamera;
+
+    public NavMeshAgent playerAgent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //gets navmesh agent component
+        playerAgent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //checks if the player press the left mouse button
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            //gets mouse position
+            Ray mousePos = worldCamera.ScreenPointToRay(Input.mousePosition);
+
+            //gets info back from mouse position raycast
+            RaycastHit mousePosHit;
+
+            //if raycast hits something
+            if(Physics.Raycast(mousePos, out mousePosHit))
+            {
+
+                //move the player to the location that got hit
+                playerAgent.SetDestination(mousePosHit.point);
+
+            }
+
+
+        }
+
+
+    }
+}
